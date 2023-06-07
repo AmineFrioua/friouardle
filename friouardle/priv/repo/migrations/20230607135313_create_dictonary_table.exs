5fa23536c65bca76879f18aaac5087330d1d2932
Mohamed Amine Frioua <aminefrioua@gmail.com>
2023-06-07 23:06:28 +0900

    create migration for dictionary

---
+++ friouardle/priv/repo/migrations/20230607135313_create_dictonary_table.exs
@@ -0,0 +1,13 @@
+defmodule Friouardle.Repo.Migrations.CreateDictonaryTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:dictonary) do
+      add :word, :string
+      add :word_length, :integer
+      add :word_letters, {:array, :char}
+      add :type, :string
+    end
+
+  end
+end
