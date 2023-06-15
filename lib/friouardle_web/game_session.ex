fb9a678da2a1b02cf8787451aecf043fcc56657d
Mohamed Amine Frioua <aminefrioua@gmail.com>
2023-06-15 23:27:09 +0900

    initial logic for wordle works

--- lib/friouardle_web/game_session.ex
+++ lib/friouardle_web/game_session.ex
@@ -14,10 +14,12 @@ defmodule FriouardleWeb.GameSession do
   def handle_event("press_key", %{"value" => key}, socket) do
     game_session = socket.assigns.game_session
     array = socket.assigns.array
-    array = check_position_and_validate(game_session, array, key)
 
-    game_session =
-      Map.update(game_session, :number_of_letters, 0, fn existing_value -> existing_value + 1 end)
+    %{game_session: game_session, array: array} =
+      case guess_or_not(game_session) do
+        :guess -> guess(game_session, array)
+        :continue -> check_position_and_validate(game_session, array, key)
+      end
 
     socket = assign(socket, game_session: game_session, array: array)
     {:noreply, socket}
@@ -26,28 +28,56 @@ defmodule FriouardleWeb.GameSession do
   def handle_event("press_key", %{"key" => key}, socket) do
     game_session = socket.assigns.game_session
     array = socket.assigns.array
-    array = check_position_and_validate(game_session, array, key)
 
-    game_session =
-      Map.update(game_session, :number_of_letters, 0, fn existing_value -> existing_value + 1 end)
+    %{game_session: game_session, array: array} =
+      case guess_or_not(game_session) do
+        :guess -> guess(game_session, array)
+        :continue -> check_position_and_validate(game_session, array, key)
+      end
 
     socket = assign(socket, game_session: game_session, array: array)
-        {:noreply, socket}
+    {:noreply, socket}
   end
 
-  defp update_game_session(game_session) do
-    tries_allowed= game_session.tries_allowed
-    number_of_letters=game_session.number_of_letters
-    number_of_tries= game_session.number_of_tries
+  def guess_or_not(game_session) do
+    IO.inspect(game_session)
+    tries_allowed = game_session.tries_allowed
+    number_of_letters = game_session.number_of_letters
+    number_of_tries = game_session.number_of_tries
+
+    result = if tries_allowed == number_of_letters, do: :guess, else: :continue
+    IO.inspect(result)
+    if tries_allowed == number_of_tries, do: :finish, else: result
+    result
   end
 
-  defp check_position_and_validate(game_session, array, key) do
+  def check_position_and_validate(game_session, array, key) do
     word =
       array
       |> Enum.at(game_session.number_of_tries)
       |> List.update_at(game_session.number_of_letters, fn _ -> {key, :a} end)
 
-    List.update_at(array, game_session.number_of_tries, fn _ -> word end)
+    game_session =
+      Map.update(game_session, :number_of_letters, 0, fn existing_value -> existing_value + 1 end)
+
+    array = List.update_at(array, game_session.number_of_tries, fn _ -> word end)
+
+    %{game_session: game_session, array: array}
+  end
+
+  def guess(game_session, array) do
+    guess =
+      array
+      |> Enum.at(game_session.number_of_tries)
+      |> Enum.map(fn {letter, _} -> letter end)
+
+    word = Wordle.guess(guess, game_session.correct_word)
+    array = List.update_at(array, game_session.number_of_tries, fn _ -> word end)
+
+    game_session =
+      Map.update(game_session, :number_of_tries, 0, fn existing_value -> existing_value + 1 end)
+
+    %{game_session: game_session, array: array}
   end
 
   defp generate_empty_tries(n) do
